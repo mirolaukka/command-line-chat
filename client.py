4a3efdcd4fa5731e71e73dd451ab406764e66d20
Miro Laukka <mjlaukka@gmail.com>
2023-07-30 12:43:13 +0300

    Documentation & Available rooms

--- client.py
+++ client.py
@@ -2,7 +2,6 @@ import os
 import socket
 import threading
 
-
 HOST = "127.0.0.1"
 PORT = 8080
 
@@ -10,15 +9,26 @@ chat_history = []
 
 
 def receive_messages(s):
-    while True:
-        message = s.recv(1024).decode()
-        if message == 'exit':
-            break
-        chat_history.append(message)
-        print_chat()
+    """
+    Receive messages from the server and display them to the user.
+
+    :param s: The server socket.
+    """
+    try:
+        while True:
+            message = s.recv(1024).decode()
+            if message == 'exit':
+                break
+            chat_history.append(message)
+            print_chat()  # Print the updated chat history
+    except (ConnectionResetError, ConnectionAbortedError):
+        pass
 
 
 def print_chat():
+    """
+    Print the entire chat history with newlines and clear the console based on the OS.
+    """
     os.system('cls' if os.name == 'nt' else 'clear')  # Clear the console based on OS
 
     for chat_msg in chat_history:
@@ -26,39 +36,38 @@ def print_chat():
 
     # Add the input prompt after printing the chat history
     print("\n> ", end="", flush=True)
-        
+
 
 def main():
-    # Implement the logic to connect to the server and handle user input here
+    """
+    Connect to the server and handle user input.
+    """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
 
         username = input("Enter your username: ")
-        room = input("Enter the chatroom name: ")
+        s.send(username.encode())
 
-        # Combine the username and room into a single string
-        user_room = f"{username}|{room}"
+        rooms_info = s.recv(1024).decode()
+        print(rooms_info)  # Display the list of available rooms and user counts
 
-        s.send(user_room.encode())
+        room = input("Enter room name: ")
+        s.send(room.encode())
 
         receive_thread = threading.Thread(target=receive_messages, args=(s,))
         receive_thread.start()
 
-
         while True:
-            
             # Print the entire chat history with newlines
             message = input("\n\n> ")
             if message == 'exit':
                 break
 
             s.send(message.encode())
-            
+
         receive_thread.join()
         s.close()
 
 
 if __name__ == '__main__':
     main()
-
-
--- server.py
+++ server.py
@@ -8,36 +8,47 @@ PORT = 8080
 # Global dictionary to store active chat rooms and their clients
 chat_rooms = {
     "general": {
-        "messages": [],
+        "messages": ["Welcome to room General!"],
         "users": []
     }
 }
 
 def handle_client(client_socket, address):
+    """
+    Function to handle a new client connection and manage the chat.
 
+    :param client_socket: The client's socket.
+    :param address: The client's address.
+    """
     def get_timestamp():
-        # Helper function to get the current timestamp
+        """Helper function to get the current timestamp in a specific format."""
         return datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
 
-    # Implement the logic to handle a new client connection here
-    user_room = client_socket.recv(1024).decode()
+    try:
+        # Implement the logic to handle a new client connection here
+        # Receive the username from the client
+        username = client_socket.recv(1024).decode()
 
-    # Separate the username and room using the delimiter '|'
-    username, room = user_room.split('|')
-    print(username, room)
-    room = room.lower()
+        # Send available rooms to the client
+        send_available_rooms(client_socket)
 
-    if room not in chat_rooms:
-        chat_rooms[room] = {"messages": [f"Welcome to room {room.capitalize()}"], "users": []}
-    chat_rooms[room]["users"].append(client_socket)
+        # Receive the room name from the client
+        room = client_socket.recv(1024).decode().lower()
 
-    # Send the chat log history to the client immediately after joining
-    send_chatlog(room, client_socket)
-    timestamp = get_timestamp()
-    chat_rooms[room]['messages'].append(f'{timestamp} {username} has joined the chat!')
-    broadcast_message(room, f'{timestamp} {username} has joined the chat!')
-    while True:
-        try:
+        # Create a new room if it doesn't exist
+        if room not in chat_rooms:
+            chat_rooms[room] = {"messages": [f"Welcome to room {room.capitalize()}!"], "users": []}
+        chat_rooms[room]["users"].append(client_socket)
+
+        # Send the chat log history to the client immediately after joining
+        send_chatlog(room, client_socket)
+
+        # Notify other users that a new client has joined
+        timestamp = get_timestamp()
+        chat_rooms[room]['messages'].append(f'{timestamp} {username} has joined the chat!')
+        broadcast_message(room, f'{timestamp} {username} has joined the chat!')
+
+        while True:
             # Receive and broadcast messages from the client
             message = client_socket.recv(1024).decode()
             if message == 'exit':
@@ -45,29 +56,59 @@ def handle_client(client_socket, address):
             timestamp = get_timestamp()
             chat_rooms[room]['messages'].append(f"{timestamp} {username}: {message}")
             broadcast_message(room, f"{timestamp} {username}: {message}")
-        except:
-            break
 
-    # Remove the client socket from the chat-room upon disconnection
-    chat_rooms[room]["users"].remove(client_socket)
-    broadcast_message(room, f"{username} has left the chat.")
-    client_socket.close()
+    except (ConnectionResetError, ConnectionAbortedError):
+        pass
+    finally:
+        # Remove the client socket from the chat-room upon disconnection
+        if room in chat_rooms:
+            chat_rooms[room]["users"].remove(client_socket)
+            broadcast_message(room, f"{username} has left the chat.")
+            client_socket.close()
+
+def send_available_rooms(client_socket):
+    """
+    Send a message to the client with the list of available rooms and user counts.
+
+    :param client_socket: The client's socket.
+    """
+    rooms_info_message = "Available Rooms:\n"
+    for room, room_data in chat_rooms.items():
+        user_count = len(room_data["users"])
+        rooms_info_message += f"- {room.capitalize()} ({user_count} users)\n"
+
+    # Send the message to the specified client
+    client_socket.send(rooms_info_message.encode())
 
 def send_chatlog(room, client):
+    """
+    Send the chat log history to the specified client.
+
+    :param room: The room for which to send the chat log.
+    :param client: The client's socket.
+    """
     for message in chat_rooms[room]['messages']:
         try:
-            client.send((message + '\n').encode())  # Append a newline character before sending
-        except:
+            # Append a newline character before sending
+            client.send((message + '\n').encode())
+        except (ConnectionResetError, ConnectionAbortedError):
             continue
 
 def broadcast_message(room, message):
+    """
+    Broadcast a message to all clients in a specific chat room.
+
+    :param room: The room in which to broadcast the message.
+    :param message: The message to be broadcasted.
+    """
     for client in chat_rooms[room]['users']:
         try:
             client.send(message.encode())
-        except:
+        except (ConnectionResetError, ConnectionAbortedError):
             continue
 
 def start_server():
+    """Function to start the chat server and listen for incoming connections."""
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
     server_socket.listen(5)
@@ -85,9 +126,6 @@ def start_server():
         print("[INFO] Server shutdown requested.")
     finally:
         server_socket.close()
-
-    
-
 
 if __name__ == '__main__':
     start_server()
